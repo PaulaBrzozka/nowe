23e6eb4ba2c8c08e80005820021e131abe0a5ee8
paula <paulinabrzozka3@gmail.com>
2022-02-12 13:10:08 +0100

    Initial commit

---
+++ nowe/src/main/java/com/example/nowe/NoweApplication.java
@@ -0,0 +1,13 @@
+package com.example.nowe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoweApplication.class, args);
+	}
+
+}
---
+++ nowe/src/test/java/com/example/nowe/NoweApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nowe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoweApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
